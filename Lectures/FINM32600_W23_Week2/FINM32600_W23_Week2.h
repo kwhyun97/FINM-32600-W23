d630284b6a75be9c02b4558adcb1922eee91f048
kwhyun97 <kwhyun97@gmail.com>
2023-01-13 18:11:40 -0600

    Week 2 initial setting

--- Lectures/FINM32600_W23_Week2/FINM32600_W23_Week2.h
+++
@@ -1,8 +0,0 @@
-﻿// FINM32600_W23_Week2.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
---
+++ Lectures/FINM32600_W23_Week2/main.cpp
@@ -0,0 +1,12 @@
+﻿// FINM32600_W23_Week2.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello CMake." << endl;
+    return 0;
+}
