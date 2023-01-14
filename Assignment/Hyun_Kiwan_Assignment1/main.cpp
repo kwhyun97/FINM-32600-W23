88fb35be01ec84ebf878659081ade686c541c80f
kwhyun97 <kwhyun97@gmail.com>
2023-01-13 21:04:05 -0600

    Assignment 1 Introduction

--- Assignment/Hyun_Kiwan_Assignment1/main.cpp
+++ Assignment/Hyun_Kiwan_Assignment1/main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Enter amount in USD: " ;
+    double amount;
+    cin >> amount;
+
+    double rate = 1.0;
+
+    enum CurrencyType {USD = 0, EUR, GBP, CAD, AUD};
+    int foreignCurrency;
+    cout << "Please Choose the desired Foreign Currency (USD = 0; EUR = 1; GBP = 2; CAD = 3; AUD = 4): ";
+    cin >> foreignCurrency;
+
+    switch(foreignCurrency)
+    {
+        case EUR:
+            rate = 0.88;
+            break;
+
+        case GBP:
+            rate = 0.73;
+            break;
+
+        case CAD:
+            rate = 1.25;
+            break;
+
+        case AUD:
+            rate = 1.39;
+            break;
+
+        default:
+            break;
+    }
+
+    cout << amount * rate << endl;
     return 0;
 }
