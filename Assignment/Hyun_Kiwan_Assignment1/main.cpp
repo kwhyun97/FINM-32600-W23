9798b80e80398ab93d58bea4332b28b4b801f8ed
Kiwan Hyun <kh303@duke.edu>
2023-01-13 23:49:23 -0600

    Assignment 1 version 1 complete

--- Assignment/Hyun_Kiwan_Assignment1/main.cpp
+++ Assignment/Hyun_Kiwan_Assignment1/main.cpp
@@ -2,40 +2,85 @@
 
 using namespace std;
 
-int main() {
-    cout << "Enter amount in USD: " ;
-    double amount;
-    cin >> amount;
+void usd_rate(const double *rates, double* rate, int n)
+{
+    *rate = rates[n];
+}
 
-    double rate = 1.0;
+int main() 
+{
+    // Introduction to the exchange
+    cout << "Valued Customer, Welcome to the Exchange!" << endl;
 
-    enum CurrencyType {USD = 0, EUR, GBP, CAD, AUD};
+    enum CurrencyType {USD = 0, EUR, GBP, CAD, AUD, JPY, RMB, EXIT};
+    int baseCurrency;
     int foreignCurrency;
-    cout << "Please Choose the desired Foreign Currency (USD = 0; EUR = 1; GBP = 2; CAD = 3; AUD = 4): ";
-    cin >> foreignCurrency;
+    double rate_1;
+    double rate_2;
+    const double ex_rate[]{ 1, 0.92, 0.82, 1.34, 1.43, 127.9, 6.7 };
+    double amount;
+    string currencies[] {"USD", "EUR", "GBP", "CAD", "AUD", "JPY", "KRW" };
 
-    switch(foreignCurrency)
+    while (1)
     {
-        case EUR:
-            rate = 0.88;
-            break;
+        cout << "Please choose your Base Currency (0 - USD; 1 - EUR; 2 - GBP; 3 - CAD; 4 - AUD; 5 - JPY; 6 - RMB; 7 - EXIT the Exchange): ";
+        cin >> static_cast<int>(baseCurrency);
 
-        case GBP:
-            rate = 0.73;
+        switch (baseCurrency)
+        {
+        case EXIT:
+            cout << "Thank you for your visit at the Exchange!" << endl;
+            return 0;
+        
+        default:
+            usd_rate(ex_rate, &rate_1, baseCurrency);
             break;
+        }
 
-        case CAD:
-            rate = 1.25;
-            break;
+        cout << "Which currency would you like to exchange to? (0 - USD; 1 - EUR; 2 - GBP; 3 - CAD; 4 - AUD; 5 - JPY; 6 - RMB; 7 - EXIT the Exchange): ";
+        cin >> static_cast<int>(foreignCurrency);
 
-        case AUD:
-            rate = 1.39;
-            break;
+        switch (foreignCurrency)
+        {
+        case EXIT:
+            cout << "Thank you for your visit at the Exchange!" << endl;
+            return 0;
 
         default:
+            usd_rate(ex_rate, &rate_2, foreignCurrency);
             break;
+        }
+
+        while (1)
+        {
+            cout << "Enter amount: ";
+            cin >> static_cast<double>(amount);
+
+            if (amount <= 0)
+            {
+                cout << "Amount must be positive. Please Try Again." << endl;
+                continue;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        cout << "" << endl;
+        cout << "------------------------------------------------" << endl;
+        cout << "Success! Your Exchange Order has been forwarded." << endl;
+        cout << "" << endl;
+        cout << "================================================" << endl;
+        cout << "Transaction Receipt: " << endl;
+        cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << endl;
+        cout << "Base Currency - " << amount << " " << currencies[baseCurrency] << endl;
+        cout << "Exchange Currency - " << amount * rate_2 / rate_1 << " " << currencies[foreignCurrency] << endl;
+        cout << "Exchange Rate: 1 " << currencies[baseCurrency] << " = " << rate_2 / rate_1 << " " << currencies[foreignCurrency] << endl;
+        cout << "================================================" << endl;
+        cout << "------------------------------------------------" << endl;
+        cout << "" << endl;
     }
 
-    cout << amount * rate << endl;
     return 0;
 }
