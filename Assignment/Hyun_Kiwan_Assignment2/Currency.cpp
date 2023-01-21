764ae2d76a0f7672bd6894023388ec50a2a370a7
kwhyun97 <kwhyun97@gmail.com>
2023-01-20 20:36:26 -0600

    Currency Class added

---
+++ Assignment/Hyun_Kiwan_Assignment2/Currency.cpp
@@ -0,0 +1,55 @@
+//
+// Created by Kiwan Hyun on 2023/01/20.
+//
+#include "Currency.h"
+
+#include <string>
+using std::string;
+
+double Currency::GetExchangeRate()
+{
+    return exchange_rate_;
+}
+
+string Currency::GetSymbol()
+{
+    return symbol_;
+}
+
+double Currency::ConvertFromUSD(double amount)
+{
+    return amount * exchange_rate_;
+}
+
+void Currency::SetSymbol(string symbol)
+{
+    symbol_ = symbol;
+}
+
+void Currency::SetCountry(string country)
+{
+    country_ = country;
+}
+
+void Currency::SetExchangeRate(double exchange_rate)
+{
+    exchange_rate_ = exchange_rate;
+}
+
+Currency::Currency()
+        : symbol_(""),
+          country_(""),
+          exchange_rate_(0.0)
+{
+}
+
+Currency::Currency(string symbol, string country, double exchange_rate)
+        : symbol_(symbol),
+          country_(country),
+          exchange_rate_(exchange_rate)
+{
+}
+
+Currency::~Currency()
+{
+}
