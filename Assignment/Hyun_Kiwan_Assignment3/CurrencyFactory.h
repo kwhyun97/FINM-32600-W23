9855499998dbd3f469841a04b99f40f6c8501b75
kwhyun97 <kwhyun97@gmail.com>
2023-02-17 23:31:39 -0600

    Austrailia -> Australia

--- Assignment/Hyun_Kiwan_Assignment3/CurrencyFactory.h
+++ Assignment/Hyun_Kiwan_Assignment3/CurrencyFactory.h
@@ -17,7 +17,7 @@ private:
     bool first_times_[7] = {true, true, true, true, true, true, true};
     string symbols_[7] = {"USD", "EUR", "GBP", "CAD", "AUD", "JPY", "RMB"};
     string countries_[7] = {"United States", "European Union", "Great Britain",
-                            "Canada", "Austailia", "Japan", "China"};
+                            "Canada", "Australia", "Japan", "China"};
     double rates_[7] = {1.0, 0.92, 0.82, 1.34, 1.43, 127.9, 6.7};
 };
 #endif //HYUN_KIWAN_ASSIGNMENT3_CURRENCYFACTOR_H
